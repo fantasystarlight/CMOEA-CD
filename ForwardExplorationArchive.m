5410406f51c2c34df30cb9d34442bee5ceab6e50
fantasystarlight <33779342+fantasystarlight@users.noreply.github.com>
2025-01-13 17:55:47 +0800

    Update ForwardExplorationArchive.m

--- ForwardExplorationArchive.m
+++ ForwardExplorationArchive.m
@@ -4,16 +4,6 @@ function FA = ForwardExplorationArchive(FA, Offspring, zmin, Ns, Add)
 % Add = 2 --- environmental selection of NSGA-II
 % Add = 3 --- environmental selection of modified NSGA-III
 
-%------------------------------- Copyright --------------------------------
-% Copyright (c) 2023 BIMK Group. You are free to use the PlatEMO for
-% research purposes. All publications which use this platform or any code
-% in the platform should acknowledge the use of "PlatEMO" and reference "Ye
-% Tian, Ran Cheng, Xingyi Zhang, and Yaochu Jin, PlatEMO: A MATLAB platform
-% for evolutionary multi-objective optimization [educational forum], IEEE
-% Computational Intelligence Magazine, 2017, 12(4): 73-87".
-%--------------------------------------------------------------------------
-
-% This function is written by Zhe Liu
     %% Dominance relation calculation
     FA = [FA, Offspring];
     NonDominated = DominationCal(FA, 0);
